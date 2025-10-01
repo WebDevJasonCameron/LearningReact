897869c1baafe2a2b6161c6b4de5524183488335
Jason Cameron <dev.jasoncameron@gmail.com>
2025-10-01 17:51:56 -0500

    fixed the error

--- TheUltimateReactCourse2025/steps/src/App.jsx
+++ TheUltimateReactCourse2025/steps/src/App.jsx
@@ -20,14 +20,13 @@ export default function App() {
     if (step < 3){
       setStep(step + 1)
     }
-
   }
 
   return <div className="steps">
     <div className="numbers">
-      <div className={`${step >= 1 ? 'active' : ""}`}>1</div>
-      <div className={`${step >= 2 ? 'active' : ""}`}>2</div>
-      <div className={`${step >= 3 ? 'active' : ""}`}>3</div>
+      <div className={step >= 1 ? 'active' : ""}>1</div>
+      <div className={step >= 2 ? 'active' : ""}>2</div>
+      <div className={step >= 3 ? 'active' : ""}>3</div>
     </div>
 
     <p className="message">Step {step}: {messages[step -1]}</p>
