7a4b012a0a5dbf7b799a18416c427d3a298fb53b
Jason Cameron <dev.jasoncameron@gmail.com>
2025-10-06 18:57:13 -0500

    slowly mv forward, after starting new job

--- TheUltimateReactCourse2025/travel-list/src/App.jsx
+++ TheUltimateReactCourse2025/travel-list/src/App.jsx
@@ -1,11 +1,31 @@
 
-function App() {
-
+export default function App() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <div>
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
   )
 }
 
-export default App
+function Logo() {
+  return <h1>🌴 Far Away 👜</h1>
+}
+
+function Form() {
+  return <div className="add-form">
+    <h3>What do you need for your 😍 trip?</h3>
+  </div>
+}
+
+function PackingList() {
+  return <div className="list">LIST</div>
+}
+
+function Stats() {
+  return <footer>
+    <em>💼You have X items on your list, and you already packed X (X%) </em>
+  </footer>
+}
