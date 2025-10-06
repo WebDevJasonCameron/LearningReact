745f4a318b0a66148ddac3823334089031389fd4
Jason Cameron <dev.jasoncameron@gmail.com>
2025-10-05 19:06:26 -0500

    start nxt travel list proj

--- TheUltimateReactCourse2025/date_calc/src/App.jsx
+++ TheUltimateReactCourse2025/date_calc/src/App.jsx
@@ -11,6 +11,7 @@ export default function App() {
 
 function Counter () {
   const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
 
   const date = new Date();
   date.setDate(date.getDate() + count);
@@ -18,9 +19,15 @@ function Counter () {
   return (
     <div>
       <div>
-        <button onClick={() => setCount(c => c - 1 )}> - </button>
+        <button onClick={() => setStep(c => c - 1 )}> - </button>
+        <span>Step: {step}</span>
+        <button onClick={() => setStep(c => c + 1 )}> + </button>
+      </div>
+
+      <div>
+        <button onClick={() => setCount(c => c - step )}> - </button>
         <span>Count: {count}</span>
-        <button onClick={() => setCount(c => c + 1 )}> + </button>
+        <button onClick={() => setCount(c => c + step )}> + </button>
       </div>
 
       <p>
