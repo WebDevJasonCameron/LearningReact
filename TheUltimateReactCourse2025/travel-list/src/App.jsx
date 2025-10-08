e626942e44d399532065d926ab9748f9529854d2
Jason Cameron <dev.jasoncameron@gmail.com>
2025-10-07 19:34:28 -0500

    continue work on the travel app practice

--- TheUltimateReactCourse2025/travel-list/src/App.jsx
+++ TheUltimateReactCourse2025/travel-list/src/App.jsx
@@ -1,7 +1,12 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Charger", quantity: 1, packed: true },
+];
 
 export default function App() {
   return (
-    <div>
+    <div className="app">
       <Logo />
       <Form />
       <PackingList />
@@ -11,21 +16,41 @@ export default function App() {
 }
 
 function Logo() {
-  return <h1>🌴 Far Away 👜</h1>
+  return <h1>🏝️ Far Away 🧳</h1>
 }
 
 function Form() {
-  return <div className="add-form">
-    <h3>What do you need for your 😍 trip?</h3>
-  </div>
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip?</h3>
+    </div>)
 }
 
 function PackingList() {
-  return <div className="list">LIST</div>
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map(item => <Item item={item} />)}
+      </ul>
+    </div>
+  )
+}
+
+function Item({item}) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: 'line-through' } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  )
 }
 
 function Stats() {
-  return <footer>
-    <em>💼You have X items on your list, and you already packed X (X%) </em>
-  </footer>
+  return (
+    <footer className="stats">
+      <em>💼You have X items on your list, and you already packed X (X%) </em>
+    </footer>
+  );
 }
