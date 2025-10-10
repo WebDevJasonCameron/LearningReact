399245afaaaeb35aaef93a910e9fd7ed4c979109
Jason Cameron <dev.jasoncameron@gmail.com>
2025-10-09 23:56:58 -0500

    cont

--- TheUltimateReactCourse2025/travel-list/src/App.jsx
+++ TheUltimateReactCourse2025/travel-list/src/App.jsx
@@ -20,8 +20,14 @@ function Logo() {
 }
 
 function Form() {
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    console.log('submit')
+  }
+
   return (
-    <form className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
       <select>
         {Array.from({ length: 20 }, (_, i) => i + 1).map(num => <option value={num} key={num}>{num}</option>)}
@@ -35,7 +41,7 @@ function PackingList() {
   return (
     <div className="list">
       <ul>
-        {initialItems.map(item => <Item item={item} />)}
+        {initialItems.map(item => <Item item={item} key={item.id} />)}
       </ul>
     </div>
   )
