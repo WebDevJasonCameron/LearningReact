6ebc55a2cf5c6ca5624efaa18ed2b2ab0e8caf8a
Jason Cameron <dev.jasoncameron@gmail.com>
2025-10-21 19:44:01 -0500

    added accord proj

---
+++ TheUltimateReactCourse2025/accordion-project/src/App.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  const faqs = [
+    {
+      title: "Where are these chairs assembled?",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+      title: " How long do I have to return my chair",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+      title: " Do you ship to countries outside the EU?",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    }
+
+  ]
+
+  return (
+    <div>
+      <Accordion />
+    </div>
+  )
+}
+
+function Accordion() {
+  return (
+    <div>
+      TODO
+    </div>
+  )
+}
+
+export default App
