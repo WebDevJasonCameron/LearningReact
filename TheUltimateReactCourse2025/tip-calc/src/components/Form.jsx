a8f6ae5ecebdf31798c5c6e4a8b3a4c52b1b2f79
Jason Cameron <dev.jasoncameron@gmail.com>
2025-10-31 11:04:50 -0500

    del con logs

--- TheUltimateReactCourse2025/tip-calc/src/components/Form.jsx
+++ TheUltimateReactCourse2025/tip-calc/src/components/Form.jsx
@@ -8,13 +8,8 @@ import {Output} from "./Output.jsx";
 
 export default function Form() {
   const [cost, setCost] = useState(0)
-  // const [totalTip, setTotalTip] = useState(0)
-  // const [total, setTotal] = useState(0)
   const [yourValue, setYourValue] = useState(0)
   const [friendsValue, setFriendsValue] = useState(0)
-
-  // setTotalTip(cost * ((yourValue + friendsValue) / 100));
-  // setTotal(cost + totalTip);
 
   function handleSubmit(event) {
     event.preventDefault()
@@ -22,17 +17,14 @@ export default function Form() {
 
   function onHandleCostChange(newCost) {
     setCost(newCost);
-    console.log("cost is: " + newCost);
   }
 
   function onHandleYourValueChange(value) {
     setYourValue(value);
-    console.log("your value is: " + value);
   }
 
   function onHandleFriendsValueChange(value) {
     setFriendsValue(value);
-    console.log("friends value is: " + value);
   }
 
   function onHandleReset() {
