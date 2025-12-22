f41181cf55c396bdd0c5315df92d2212a6bb0bf1
Jason Cameron <dev.jasoncameron@gmail.com>
2025-12-22 06:21:21 -0600

    working on star rating comp

--- TheUltimateReactCourse2025/use-popcorn/src/main.jsx
+++ TheUltimateReactCourse2025/use-popcorn/src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import StarRating from "./components/StarRating.jsx";
+// import './index.css'
+// import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <StarRating />
+    {/*<App />*/}
   </StrictMode>,
 )
