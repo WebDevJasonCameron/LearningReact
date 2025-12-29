7f180a27b4f710cab9718b64119ed993739ec587
Jason Cameron <dev.jasoncameron@gmail.com>
2025-12-29 12:18:48 -0600

    star comp compl. need to add to app

--- TheUltimateReactCourse2025/use-popcorn/src/components/StarRating.jsx
+++ TheUltimateReactCourse2025/use-popcorn/src/components/StarRating.jsx
@@ -33,7 +33,9 @@ export default function StarRating({ maxRating = 5 }) {
     <div style={containerStyle}>
       <div style={starContainerStyle}>
         {Array.from({ length: maxRating }, (_, i) => (
-         <Star key={i} onRate={() => handleRating(i + 1)} />
+         <Star key={i}
+               onRate={() => handleRating(i + 1)}
+               full={rating >= i + 1} />
         ))}
       </div>
       <p style={textStyle}>{rating || ""}</p>
