7843512553a30fbdd2371e4875943ad19ea18c60
Jason Cameron <dev.jasoncameron@gmail.com>
2026-01-09 21:30:23 -0600

    txt expander compl

--- TheUltimateReactCourse2025/text-expander/src/comps/TextExpander.jsx
+++ TheUltimateReactCourse2025/text-expander/src/comps/TextExpander.jsx
@@ -1,24 +1,32 @@
 import {useState} from "react";
 
-export default function TextExpander({ collapsedNumWords,
+export default function TextExpander({ collapsedNumWords = 10,
                                        expandButtonText = "Show more",
-                                       collapseButtonText,
-                                       buttonColor,
+                                       collapseButtonText = "Show less",
+                                       buttonColor = '#1f09cd',
                                        expanded = false,
                                        className,
                                        children }) {
 
   const [ isExpanded, setIsExpanded ] = useState(expanded);
-
-  const displayText = isExpanded ? children : "test";
+  const displayText = isExpanded ? children : children.split(' ').slice(0, collapsedNumWords).join(' ') + '...';
+  const buttonStyle = {
+    background: 'none',
+    border: 'none',
+    font: 'inherit',
+    cursor: 'pointer',
+    marginLeft: '6px',
+    color: buttonColor
+  }
 
   return (
     <div className={className}>
       <span>
         { displayText }
       </span>
-      <button>
-        { expandButtonText }
+      <button onClick={() => setIsExpanded((exp) => !exp)}
+              style={buttonStyle}>
+        { isExpanded ? collapseButtonText : expandButtonText }
       </button>
     </div>);
 }
