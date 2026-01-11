7e85fd26757d935ca3af74b5242f8748d92d6fe3
Jason Cameron <dev.jasoncameron@gmail.com>
2026-01-11 14:30:38 -0600

    cut into comps

--- TheUltimateReactCourse2025/how-react-works/src/assets/Content.jsx
+++ TheUltimateReactCourse2025/how-react-works/src/assets/Content.jsx
@@ -1,5 +1,3 @@
-import Tabbed from "./comps/Tabbed";
-
 const content = [
   {
     summary: "React is a library for building UIs",
@@ -17,12 +15,3 @@ const content = [
       "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
   },
 ];
-
-export default function App() {
-  return (
-    <div>
-      <Tabbed content={content} />
-    </div>
-  );
-}
-
---
+++ TheUltimateReactCourse2025/how-react-works/src/comps/Tabbed.jsx
@@ -0,0 +1,25 @@
+import Tab from "./Tab.jsx";
+import TabContent from "./TabContent.jsx";
+import DifferentContent from "./DifferentContent.jsx";
+import {useState} from "react";
+
+export default function Tabbed({ content }) {
+  const [activeTab, setActiveTab] = useState(0);
+
+  return (
+    <div>
+      <div className="tabs">
+        <Tab num={0} activeTab={activeTab} onClick={setActiveTab} />
+        <Tab num={1} activeTab={activeTab} onClick={setActiveTab} />
+        <Tab num={2} activeTab={activeTab} onClick={setActiveTab} />
+        <Tab num={3} activeTab={activeTab} onClick={setActiveTab} />
+      </div>
+
+      {activeTab <= 2 ? (
+        <TabContent item={content.at(activeTab)} />
+      ) : (
+        <DifferentContent />
+      )}
+    </div>
+  );
+}
