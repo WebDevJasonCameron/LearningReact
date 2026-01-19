c8e0aff002d3ff8db70810b047f5a34c7af9b60f
Jason Cameron <dev.jasoncameron@gmail.com>
2026-01-19 05:30:06 -0600

    adding keys to fix non refresh error

--- TheUltimateReactCourse2025/eate-and-split/src/App.jsx
+++ TheUltimateReactCourse2025/eate-and-split/src/App.jsx
@@ -4,27 +4,7 @@ import Button from "./components/Button.jsx";
 import FormSplitBill from "./components/FormSplitBill.jsx";
 import {useState} from "react";
 
-const initialFriends = [
-  {
-    id: 118836,
-    name: "Clark",
-    image: "https://i.pravatar.cc/48?u=118836",
-    balance: -7,
-  },
-  {
-    id: 933372,
-    name: "Sarah",
-    image: "https://i.pravatar.cc/48?u=933372",
-    balance: 20,
-  },
-  {
-    id: 499476,
-    name: "Anthony",
-    image: "https://i.pravatar.cc/48?u=499476",
-    balance: 0,
-  },
-];
-
+import {initialFriends} from "./assets/InitialFriends.jsx";
 
 function App() {
   const [friends, setFriends] = useState(initialFriends);
@@ -66,13 +46,14 @@ function App() {
 
         {showAddFriend && <FormAddFriend onAddFriend={ handleAddFriend } />}
 
-        <Button onClick={handleShowAddFriend} >
+        <Button onClick={ handleShowAddFriend } >
           { showAddFriend ? "Close" : "Add friend" }
         </Button>
       </div>
 
       { selectedFriend && <FormSplitBill selectedFriend={ selectedFriend }
-                                         onSplitBill={handleSplitBill} /> }
+                                         onSplitBill={ handleSplitBill }
+                                         key={ selectedFriend.id } /> }
     </div>
   )
 }
