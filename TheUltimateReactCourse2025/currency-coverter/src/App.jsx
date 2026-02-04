6add83838202488863d7f474a22a3dcdc482eb46
Jason Cameron <dev.jasoncameron@gmail.com>
2026-02-04 06:15:52 -0600

    work, but amount does not change

--- TheUltimateReactCourse2025/currency-coverter/src/App.jsx
+++ TheUltimateReactCourse2025/currency-coverter/src/App.jsx
@@ -1,20 +1,51 @@
+// `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
+
+import {useEffect, useState} from "react";
+
 export default function App() {
+  /**
+   * USs
+   */
+  const [amount, setAmount] = useState(1);
+  const [fromCur, setFromCur] = useState("EUR");
+  const [toCur, setToCur] = useState("USD");
+  const [converted, setConverted] = useState("");
+
+  /**
+   * UEs
+   */
+  useEffect(function () {
+    async function convert() {
+      const res = await fetch(
+        `https://api.frankfurter.app/latest?${amount}=100&from=${fromCur}&to=${toCur}`,
+      )
+      const data = await res.json();
+      setConverted(data.rates[toCur]);
+    }
+
+    convert();
+  }, [amount, fromCur, toCur])
+
   return (
     <div>
-      <input type="text" />
-      <select>
+      <input type="text"
+             value={amount}
+             onChange={(e) => setAmount(Number(e.target.value))} />
+      <select value={fromCur}
+              onChange={(e) => setFromCur(e.target.value)}>
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <select>
+      <select value={toCur}
+              onChange={(e) => setToCur(e.target.value)}>
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <p>OUTPUT</p>
+      <p>{converted} {toCur}</p>
     </div>
   );
 }
