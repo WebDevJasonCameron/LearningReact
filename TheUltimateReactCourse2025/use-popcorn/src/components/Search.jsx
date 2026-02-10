7daa76e6b5fe1fb346b7f2ee3edaa9302ae7b19c
Jason Cameron <dev.jasoncameron@gmail.com>
2026-02-09 20:00:41 -0600

    now using the useRef hook

--- TheUltimateReactCourse2025/use-popcorn/src/components/Search.jsx
+++ TheUltimateReactCourse2025/use-popcorn/src/components/Search.jsx
@@ -1,12 +1,22 @@
+import {useEffect, useRef} from "react";
+
 export default function Search({ query, setQuery }) {
+  const inputElement = useRef(null);
+
+  useEffect(() => {
+    inputElement.current.focus();
+    console.log(inputElement.current)
+  }, [])
 
   return (
     <input
       className="search"
+      id="search-input"
       type="text"
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputElement}
     />
   )
 }
