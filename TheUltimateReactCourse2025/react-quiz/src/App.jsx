f7aa11205660b0b1887dd35858b6fa6de61d8176
Jason Cameron <dev.jasoncameron@gmail.com>
2026-02-26 19:57:32 -0600

    used an npm server to mock api

--- TheUltimateReactCourse2025/react-quiz/src/App.jsx
+++ TheUltimateReactCourse2025/react-quiz/src/App.jsx
@@ -1,12 +1,52 @@
 import './index.css'
-import DataCounter from "./DataCounter.jsx";
+import Header from "./comps/Header.jsx";
+import Main from "./comps/Main.jsx";
+import {useEffect, useReducer} from "react";
+
+const initialState = {
+  questions: [],
+
+  // 'loading', 'error', 'ready', 'active', 'finished'
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      }
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      }
+    default:
+      throw new Error(`Unknown action type ${action.type}`);
+  }
+}
 
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: 'dataFailed' }));
+  }, []);
 
   return (
-    <>
-      <DataCounter />
-    </>
-  )
+    <div className="app">
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Questions?</p>
+      </Main>
+
+    </div>
+  );
 }
 
