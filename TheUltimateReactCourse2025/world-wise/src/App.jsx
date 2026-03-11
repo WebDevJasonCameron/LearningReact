6ecac4d97d98a70da1a02d0126cceb887e78e0c8
Jason Cameron <dev.jasoncameron@gmail.com>
2026-03-10 19:56:30 -0500

    routes added

--- TheUltimateReactCourse2025/world-wise/src/App.jsx
+++ TheUltimateReactCourse2025/world-wise/src/App.jsx
@@ -1,9 +1,23 @@
 import './App.css'
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Product from "./pages/Product.jsx";
+import Pricing from "./pages/Pricing.jsx";
+import Homepage from "./pages/Homepage.jsx";
 
 function App() {
+
   return (
     <>
-      <h1>World Wise</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/"
+                 element={ <Homepage /> } />
+          <Route path="product"
+                 element={ <Product /> } />
+          <Route path="pricing"
+                 element={ <Pricing /> } />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
