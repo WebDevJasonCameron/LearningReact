2809c36d81d1bf09c4055cf7574e5b2add397401
Jason Cameron <dev.jasoncameron@gmail.com>
2026-03-11 17:44:25 -0500

    page not found in route

--- TheUltimateReactCourse2025/world-wise/src/App.jsx
+++ TheUltimateReactCourse2025/world-wise/src/App.jsx
@@ -3,6 +3,7 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Product from "./pages/Product.jsx";
 import Pricing from "./pages/Pricing.jsx";
 import Homepage from "./pages/Homepage.jsx";
+import PageNotFound from "./pages/PageNotFound.jsx";
 
 function App() {
 
@@ -16,6 +17,8 @@ function App() {
                  element={ <Product /> } />
           <Route path="pricing"
                  element={ <Pricing /> } />
+          <Route path="*"
+                 element={ <PageNotFound /> }  />
         </Routes>
       </BrowserRouter>
     </>
