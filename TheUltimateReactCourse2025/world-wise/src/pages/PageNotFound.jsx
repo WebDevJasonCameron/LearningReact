ade79f824a6a28de7868f2f0baeb93960ea49278
Jason Cameron <dev.jasoncameron@gmail.com>
2026-03-14 08:21:28 -0500

    rm starter code to follow better

--- TheUltimateReactCourse2025/world-wise/src/pages/PageNotFound.jsx
+++ TheUltimateReactCourse2025/world-wise/src/pages/PageNotFound.jsx
@@ -1,7 +1,11 @@
+import PageNav from "../components/PageNav.jsx";
+
 export default function PageNotFound() {
   return (
-    <div>
+    <>
       <h1>Page not found 😢</h1>
-    </div>
+      <PageNav />
+
+    </>
   );
 }
