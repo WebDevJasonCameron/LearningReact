21e414b119bb3432009f7edf146cec293c5a129c
Jason Cameron <dev.jasoncameron@gmail.com>
2026-03-20 11:07:31 -0500

    cities added with iniitial blank accounted for

--- TheUltimateReactCourse2025/world-wise/src/components/CityList.jsx
+++ TheUltimateReactCourse2025/world-wise/src/components/CityList.jsx
@@ -1,9 +1,11 @@
 import styles from "./CityList.module.css"
 import Spinner from "./Spinner.jsx";
 import CityItem from "./CityItem.jsx";
+import Message from "./Message.jsx";
 
 export default function CityList({ cities, isLoading } ) {
   if(isLoading) return <Spinner />
+  if(!cities.length) return <Message message="Add your first city by clicking on city on the map" />;
 
   return (
     <ul className={ styles.cityList }>
